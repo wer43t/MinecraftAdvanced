1e2837e9c60ebcaf79ba60d40b3dc0a02d70feb3
Михаил Мясников <gutufik@mail.ru>
2022-03-12 09:23:41 +0300

    Added monkeys

--- MinecraftAdvanced/MinecraftAdvanced/Views/AboutPage.xaml.cs
+++ MinecraftAdvanced/MinecraftAdvanced/Views/AboutPage.xaml.cs
@@ -2,14 +2,42 @@
 using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
 
 namespace MinecraftAdvanced.Views
 {
     public partial class AboutPage : ContentPage
     {
+        public List<Monkey> Monkeys { get; set; }
         public AboutPage()
         {
             InitializeComponent();
+            Monkeys = new List<Monkey>();
+            Monkeys.Add(new Monkey() 
+            { 
+                Name = "Andrew", 
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg",
+                Location = "Kazan",
+                Details = "Bozin"
+            }
+            );
+            Monkeys.Add(new Monkey()
+            {
+                Name = "Andrew",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg",
+                Location = "Kazan",
+                Details = "Bozin"
+            }
+            );
+
+            this.BindingContext = this;
         }
+    }
+    public class Monkey
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
     }
 }
