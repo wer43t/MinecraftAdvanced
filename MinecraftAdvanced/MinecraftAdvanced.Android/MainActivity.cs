5515037f9f7179c5d55731c21c01cf15adcc6e5a
Михаил Мясников <gutufik@mail.ru>
2022-03-16 07:00:20 +0300

    updated flayout header

--- MinecraftAdvanced/MinecraftAdvanced.Android/MainActivity.cs
+++ MinecraftAdvanced/MinecraftAdvanced.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace MinecraftAdvanced.Droid
 {
-    [Activity(Label = "MinecraftAdvanced", Icon = "@mipmap/AdvancedIcon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "MinecraftAdvanced", Icon = "@mipmap/AdvancedIcon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
