3105e5bb1092f1d8d60973ea7aae3c392154abba
Михаил Мясников <gutufik@mail.ru>
2022-03-20 12:10:10 +0300

    animated splash screen

--- MinecraftAdvanced/MinecraftAdvanced.Android/MainActivity.cs
+++ MinecraftAdvanced/MinecraftAdvanced.Android/MainActivity.cs
@@ -7,15 +7,16 @@ using Android.OS;
 
 namespace MinecraftAdvanced.Droid
 {
-    [Activity(Label = "MinecraftAdvanced", Icon = "@mipmap/AdvancedIcon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "MinecraftAdvanced", Icon = "@mipmap/AdvancedIcon", Theme = "@style/MainTheme", MainLauncher = false, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ MinecraftAdvanced/MinecraftAdvanced.Android/SplashActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Com.Airbnb.Lottie;
+using Android.Animation;
+
+namespace MinecraftAdvanced.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", Label = "SplashActivity", MainLauncher =true, NoHistory =true)]
+    public class SplashActivity : Activity, Animator.IAnimatorListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Splash_Layout);
+
+            var animationView = FindViewById<LottieAnimationView>(Resource.Id.animation_view);
+            animationView.AddAnimatorListener(this);
+        }
+        public void OnAnimationCancel(Animator animation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnAnimationEnd(Animator animation)
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public void OnAnimationRepeat(Animator animation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnAnimationStart(Animator animation)
+        {
+            
+        }
+
+        
+    }
+}
